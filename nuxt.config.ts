ca0994d963a7d976ca8905a0b06d9f097b021b7d
gumingWu <15626185054@163.com>
2022-11-27 23:33:17 +0800

    使用docus，现版本解决window不能启动问题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: [
+    '@nuxt-themes/docus'
+  ],
   app: {
     head: {
       viewport: 'width=device-width, initial-scale=1',
       title: 'DailyHand',
       charset: 'utf-8',
+      link: [
+        {
+          rel: 'icon',
+          href: '/favicon.svg'
+        }
+      ],
       meta: [
-        {name: 'description', content: '可能是现代最全的JS手写题文档'}
+        {name: 'description', content: '可能是现代最全的JS手写题文档'},
+        {name: 'keywords', content: 'js,手写题,面试题,手写题文档,handwrite'},
+        {name: 'author', content: 'gaaming'}
       ]
     }
   }
